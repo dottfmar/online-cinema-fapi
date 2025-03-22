4cc885b4be15b2227fed831dffe63e1ad488ba48
Mariana Lishchynska <lishchynska.m@gmail.com>
2025-03-22 13:47:29 +0200

    decompose code

---
+++ src/database/models/associations.py
@@ -0,0 +1,28 @@
+# isort: skip_file
+
+from sqlalchemy import Column, ForeignKey, Table
+
+from src.database.models.base import Base
+
+MoviesGenresModel = Table(
+    "movie_genres",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id", ondelete="CASCADE"), primary_key=True),
+    Column("genre_id", ForeignKey("genres.id", ondelete="CASCADE"), primary_key=True),
+)
+
+StarsMoviesModel = Table(
+    "movie_stars",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id", ondelete="CASCADE"), primary_key=True),
+    Column("star_id", ForeignKey("stars.id", ondelete="CASCADE"), primary_key=True),
+)
+
+MoviesDirectorsModel = Table(
+    "movie_directors",
+    Base.metadata,
+    Column("movie_id", ForeignKey("movies.id", ondelete="CASCADE"), primary_key=True),
+    Column(
+        "director_id", ForeignKey("directors.id", ondelete="CASCADE"), primary_key=True
+    ),
+)

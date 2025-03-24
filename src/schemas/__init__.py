211d7423c9485216a5b4fc60c9e0087a788a078d
Lisa <zhyganiuk.elizaveta@gmail.com>
2025-03-24 17:13:11 +0200

    added notifications, exceptions, storages, interfaces for security, fixed __init__ in config, database and schemas

---
+++ src/schemas/__init__.py
@@ -0,0 +1,14 @@
+# isort: skip_file
+# flake8: noqa: F401
+from schemas.accounts import (
+    MessageResponseSchema,
+    PasswordResetCompleteRequestSchema,
+    PasswordResetRequestSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema,
+    UserActivationRequestSchema,
+    UserLoginRequestSchema,
+    UserLoginResponseSchema,
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+)
